64c26ca2f873740fb68ebe9895b9fe1db381d0ed
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-24 23:53:50 +0100

    trying corrida

--- Trabalho1/helloworld.c
+++ Trabalho1/helloworld.c
@@ -5,18 +5,16 @@
 #define NUM_THREADS 5
 
 void* hello(void * thread) {
- printf("HELLO WORLD BY THREAD %ld",(long)thread);
+ printf("HELLO WORLD BY THREAD %ld\n",(long)thread);
  pthread_exit(NULL);
 }
-
-
-
-
+pthread_t threads[NUM_THREADS];
+int i=0;
 int main() {
-  pthread_t threads[NUM_THREADS];
+  
 
 
-  for(int i=0;i<NUM_THREADS;++i){
+  for(i=0;i<NUM_THREADS;++i){
 
     int res = pthread_create(&threads[i], NULL, hello, (void*)i);
     if(res){
