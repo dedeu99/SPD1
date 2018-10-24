ef1c9a87b6d8f3874648b2f7b5615bfdfc01e67e
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-24 23:39:28 +0100

    trying corrida

--- Trabalho1/helloworld.c
+++ Trabalho1/helloworld.c
@@ -3,15 +3,15 @@
 #include <pthread.h>
 
 void* hello(void * thread) {
- printf("HELLO WORLD BY THREAD %d",(int)thread);
+ printf("HELLO WORLD BY THREAD %ld",(long)thread);
 }
 
 
 int main() {
   pthread_t t;
-  int res = pthread_create(&t, NULL, hello, t);
+  int res = pthread_create(&t, NULL, hello, (void*)t);
   if(res){
-    printf("ERROR:     return code from pthread_create() is %d\n", errcode);
+    printf("ERROR:     return code from pthread_create() is %d\n", res);
     exit(-1);
   }
 
