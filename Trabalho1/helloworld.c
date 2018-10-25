5dadcf087c97a5de6f56d7d2f78b4338a11a10ef
dedeu99 <andre.calixto.martins@gmail.com>
2018-10-25 09:41:08 +0100

    removing fork from examples

--- Trabalho1/helloworld.c
+++ Trabalho1/helloworld.c
@@ -5,7 +5,6 @@
 
 
 void* hello(void * thread) {
-  sleep(1);
   printf("HELLO WORLD BY THREAD %ld\n",(long)thread);
 
   pthread_exit(NULL);
--- Trabalho1/mutexes.c
+++ Trabalho1/mutexes.c
@@ -48,9 +48,6 @@ int main(int argc,  char** argv) {
 	      exit(-1);
 	    }    
 	}	
-	for(int t=0;t<nthreads;++t)
-  		pthread_join(threads[t], NULL);
-
   	printf("Number of threads:%d   incremental result:%d\n",nthreads,i);
   	pthread_mutex_destroy(&i_mutex);
 	pthread_exit(NULL);
